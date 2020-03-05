8aea201ce412ab58f46939561ab05cf26d2b047e
Lian Y <lian.yao@gmx.de>
2020-03-05 10:54:33 +0100

    Steuerklasse

--- src/edu/campus02/Main.java
+++ src/edu/campus02/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
 
         // if (steuer1 > 0.1999 && steuer1 < 0.20001)
-        if (steuerklasse == 1) {
+       /* if (steuerklasse == 1) {
             steuer =nettoPreis * steuer1;
             System.out.println("Steuersatz ist in Steuerklasse 1!");
         } else if (steuerklasse == 2) {
@@ -24,6 +24,26 @@ public class Main {
             steuer =nettoPreis * steuer3;
             System.out.println("Steuersatz ist in Steuerklasse 3!");
         }
+        bruttoPreis = nettoPreis + steuer;
+        System.out.println("Der Bruttopreis ist: " + bruttoPreis);*/
+
+       switch (steuerklasse) {
+           case 1:
+               steuer =nettoPreis * steuer1;
+               System.out.println("Steuersatz ist in Steuerklasse 1!");
+               break;
+           case 2:
+               steuer =nettoPreis * steuer2;
+               System.out.println("Steuersatz ist in Steuerklasse 2!");
+               break;
+           case 3:
+               steuer =nettoPreis * steuer3;
+               System.out.println("Steuersatz ist in Steuerklasse 3!");
+               break;
+           default:
+               System.out.println("Keine Steuerklasse");
+               break;
+       }
         bruttoPreis = nettoPreis + steuer;
         System.out.println("Der Bruttopreis ist: " + bruttoPreis);
     }
